2bd1c3a52b3b2717283939097d0f7ac0d83090b8
yuvraj-3 <yuvrajkumarsingh303@gmail.com>
2024-03-02 00:54:33 +0530

    reverse inverted triangle

---
+++ pattern3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "Enter number of rows : " ;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int k = i; k > 1; k--)
+		{
+			cout << " ";
+		}
+		
+		for (int j = n - i; j >= 0; j--)
+		{
+			cout <<"*";
+		}
+		cout << endl;
+	}
+	return 0;
+	
+}
